abadf26470bd7e202341abfcd6cd09c2f8c4def2
Michael Zheng <michaelzheng@Michaels-MacBook-Air.local>
2021-10-22 14:46:52 -0400

    adding code completing the 'Templates and Views' section of the README.md document of https://github.com/mjhea0/flaskr-tdd

--- project/app.py
+++ project/app.py
@@ -1,10 +1,13 @@
 #this is app.py
 
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
 
 #configuration
 DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "change_me"
 
 #create and initialize a new Flask app
 app = Flask(__name__)
@@ -41,10 +44,54 @@ def close_db(error):
 		g.sqlite_db.close()
 
 
-@app.route("/")
+#replacing the "hello" view with an "index" view
+@app.route('/')
+def index():
+    """Searches the database for entries, then displays them."""
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
 
-def hello():
-	return "Hello, World!"
+# from the README, the "methods=['GET', 'POST'] argument to @app.route indicates that this route can accept GET and POST requests
+# from the README: users use GET requests to access the webage, POST requests when trying to log in
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
 	app.run()
